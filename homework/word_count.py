109de31a9e9b3a34f19c865f61c4ccbcedb1f56d
Anny <anberrio@unal.edu.co>
2025-04-26 14:35:24 -0500

    actualizacion

--- homework/word_count.py
+++ homework/word_count.py
@@ -9,7 +9,7 @@ from itertools import groupby
 
 
 #
-# Escriba la función load_input que recive como parámetro un folder y retorna
+# Escriba la función load_input que recibe como parámetro un folder y retorna
 # una lista de tuplas donde el primer elemento de cada tupla es el nombre del
 # archivo y el segundo es una línea del archivo.
 #
